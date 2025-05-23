78ceee173a31b5ca9ea0119ac66b225b6ac0cf44
KatRose7 <rgonzalesch@fcpn.edu.bo>
2025-05-23 00:26:34 -0400

    Producto agregado

--- src/main/java/com/plataforma/ecommerce/model/Categoria.java
+++ src/main/java/com/plataforma/ecommerce/model/Categoria.java
@@ -1,5 +1,8 @@
 package com.plataforma.ecommerce.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.*;
 
@@ -33,4 +37,8 @@ public class Categoria {
     @ManyToOne
     @JoinColumn(name = "id_catalogo", nullable = false)
     private Catalogo catalogo;
+
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Producto> productos;
+   
 }
--- src/main/java/com/plataforma/ecommerce/model/Producto.java
+++ src/main/java/com/plataforma/ecommerce/model/Producto.java
@@ -1,5 +1,56 @@
 package com.plataforma.ecommerce.model;
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "producto")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Producto {
-    int numumero = 0; 
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private Date aniodeongreso;
+
+    private String descripcion;
+
+    @Column(nullable = false)
+    private int stock;
+
+    @Column(nullable = false)
+    private int stock_minimo;
+
+    @Column(nullable = false)
+    private int unidad_medida;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria", nullable = false)
+    private Catalogo catalogo;
 }
+
+    
+
