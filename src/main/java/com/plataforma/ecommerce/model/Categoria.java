b30a96c4c97452302d166a01f26163b2b9eee93f
napoleonbaley2011 <lchamacal@fcpn.edu.bo>
2025-05-22 23:36:17 -0400

    Cambios Categoria

---
+++ src/main/java/com/plataforma/ecommerce/model/Categoria.java
@@ -0,0 +1,30 @@
+package com.plataforma.ecommerce.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+
+@Entity
+@Table(name = "categoria")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    private String descripcion;
+}
